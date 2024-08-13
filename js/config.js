867eb86728c22b22393a541c73b7eaa5aeb01079
kaito <zax04097@outlook.jp>
2024-08-13 18:25:24 +0900

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-3_H1Cv8qein', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5n8b24p4e5sh71bme44q4ni1mf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-3' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
