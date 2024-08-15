7f096923563b552cc888e716c5762db2cd418b80
kaito <zax04097@outlook.jp>
2024-08-15 21:29:44 +0900

    new_configuration

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-northeast-3' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://chorwre05b.execute-api.ap-northeast-3.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
